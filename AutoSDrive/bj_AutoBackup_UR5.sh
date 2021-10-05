71b575a693617ca6658ac87f3142f2444cec6504
brandon <test@test>
2021-10-05 17:10:46 +1100

    ur5 backup locations

--- AutoSDrive/bj_AutoBackup_UR5.sh
+++ AutoSDrive/bj_AutoBackup_UR5.sh
@@ -18,7 +18,10 @@ destination="/run/user/1000/gvfs/smb-share:server=ad.monash.edu,share=shared/RoM
 
 # Locations to backup
 declare -a sourceList=(
-    "$HOME/brandon_ws/CraneExp/pure"
+    "$HOME/brandon_ws/CraneExp/scripts"
+    "$HOME/brandon_ws/CraneExp/pure/src"
+    "$HOME/brandon_ws/CraneExp/pure/bin"
+    "$HOME/brandon_ws/CraneExp/catkin/src/bj_ur5_gripper"
     "$HOME/brandon_ws/CraneExp/lib_ubuntu"
 )
 
